06d8c73f11e248bda13822632a88642e5ee82436
SRenan <renan.sauteraud@gmail.com>
2016-01-14 20:48:36 +0000

    Filter GSEAR by cohorts, not container

--- DifferentialExpressionAnalysis/queries/gene_expression/DGEA_filteredGEAR.sql
+++ DifferentialExpressionAnalysis/queries/gene_expression/DGEA_filteredGEAR.sql
@@ -16,5 +16,6 @@ gene_expression_analysis,
 gene_expression_analysis_results
 WHERE
 gene_expression_analysis_results.container IN (SELECT Container FROM study.participant)
+AND gene_expression_analysis.arm_name IN (SELECT name FROM study.cohort_membership)
 AND gene_expression_analysis_results.analysis_accession = gene_expression_analysis.analysis_accession
 AND gene_expression_analysis_results.container = gene_expression_analysis.container
--- Scripts/mkStudyFolderTree.sh
+++ Scripts/mkStudyFolderTree.sh
@@ -2,15 +2,15 @@ if [ $1 ] ; then
   study=$1
   folder="/share/files/Studies/${study}/@files/"
 
-  echo "\nNew folders will be created under: ${folder}"
+  echo "New folders will be created under: ${folder}"
 
-  mkdir ${folder}cache
-  mkdir ${folder}protocols
-  mkdir ${folder}rawdata
-  mkdir ${folder}rawdata/gene_expression
-  mkdir ${folder}rawdata/flow_cytometry
-  mkdir ${folder}analysis
-  mkdir ${folder}analysis/exprs_matrices
+  mkdir ${folder}cache &> /dev/null
+  mkdir ${folder}protocols &> /dev/null
+  mkdir ${folder}rawdata &> /dev/null
+  mkdir ${folder}rawdata/gene_expression &> /dev/null
+  mkdir ${folder}rawdata/flow_cytometry &> /dev/null
+  mkdir ${folder}analysis &> /dev/null
+  mkdir ${folder}analysis/exprs_matrices &> /dev/null
 else
   echo "The first argument should be a study accession number"
 fi
