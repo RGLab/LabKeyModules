02b555d470685136b94b5f33b62032a31d39af1c
SRenan <renan.sauteraud@gmail.com>
2016-02-17 18:12:35 +0000

    Add view and R code to handle GEO based GEM

--- HIPCMatrix/pipeline/tasks/create-matrix.r
+++ HIPCMatrix/pipeline/tasks/create-matrix.r
@@ -9,9 +9,16 @@ jobInfo <- read.table("${pipeline, taskInfo}",
                       header=FALSE, check.names=FALSE,
                       stringsAsFactors=FALSE, sep="\t", quote="",
                       fill=TRUE, na.strings="")
+## Handle ALL inputs (useful for working locally on the code)
+labkey.url.base <- jobInfo$value[jobInfo$name == "baseUrl"]
+labkey.url.path <- jobInfo$value[jobInfo$name == "containerPath"]
+pipeline.root   <- jobInfo$value[jobInfo$name == "pipeRoot"]
+inputFiles      <- jobInfo$value[ grep("input\\.", jobInfo$name)]
 
-
+selectedBiosamples <- selectedGEOs <- NULL
+# From LABKEY.Pipeline.startAnalysis in views/CreateMatrix.html
 selectedBiosamples <- "${selected-biosamples}"
+selectedGEOs <- "${selected-GEOs}"
 
 library(ImmuneSpaceR)
 
@@ -22,20 +29,24 @@ library(ImmuneSpaceR)
 
 
 # @value A data.table with a feature_id column and one column per biosample_accession
-normalizeMatrix <- function(jobInfo, selectedBiosamples){
-  labkey.url.base <- jobInfo$value[jobInfo$name == "baseUrl"]
-  labkey.url.path <- jobInfo$value[jobInfo$name == "containerPath"]
+normalizeMatrix <- function(labkey.url.base, labkey.url.path, inputFiles, selectedBiosamples, selectedGEOs){
   con <- CreateConnection()
-  inputFiles <- jobInfo$value[ grep("input\\.", jobInfo$name)]
-
   inputFiles <- inputFiles[file.exists(inputFiles)]
-  filter <- makeFilter(c("file_info_name", "IN", paste(basename(inputFiles), collapse=";")),
-                       c("biosample_accession", "IN", gsub(",", ";", selectedBiosamples)))
+  GEOs <- unlist(strsplit(selectedGEOs, ","))
+  filter <- makeFilter(c("biosample_accession", "IN", gsub(",", ";", selectedBiosamples)))
   gef <- con$getDataset("gene_expression_files", colFilter = filter, original_view = TRUE, reload = TRUE)
+  gef <- gef[file_info_name %in% basename(inputFiles) | geo_accession %in% GEOs]
+  # Decide whether we use GEO or the files
+  if(all(GEOs %in% gef$geo_accession)){
+    isGEO <- TRUE
+  } else if(all(basename(inputFiles) %in% gef$file_info_name)){
+    isGEO <- FALSE
+  } else{
+    stop("Could not decide between GEO and files. Check selected rows to make sure that all selected rows have a file or they all have a GEO accession number.")
+  }
 
-  gef <- gef[file_info_name %in% basename(inputFiles) | file_info_purpose == 'GEO link']
   cohort <<- unique(gef$cohort)
-  norm_exprs <- con$makeMatrix(gef)
+  norm_exprs <- con$makeMatrix(gef, isGEO)
   return(norm_exprs)
 }
 
@@ -49,45 +60,39 @@ summarizeMatrix <- function(exprs, f2g){
   return(em)
 }
 
-writeMatrix <- function(exprs, bygene){
+writeMatrix <- function(pipeline.root, exprs, bygene){
   # - EM
   setnames(exprs, "feature_id", " ")
-  write.table(exprs, file = file.path(jobInfo$value[jobInfo$name == "pipeRoot"], "analysis/exprs_matrices", "${output.tsv}"), sep = "\t", quote=FALSE, row.names=FALSE)
+  write.table(exprs, file = file.path(pipeline.root, "analysis/exprs_matrices", "${output.tsv}"), sep = "\t", quote=FALSE, row.names=FALSE)
   # - summary EM
-  write.table(bygene, file = file.path(jobInfo$value[jobInfo$name == "pipeRoot"], "analysis/exprs_matrices", paste0("${output.tsv}", ".summary")), sep = "\t", quote=FALSE, row.names=FALSE)
+  write.table(bygene, file = file.path(pipeline.root, "analysis/exprs_matrices", paste0("${output.tsv}", ".summary")), sep = "\t", quote=FALSE, row.names=FALSE)
   # - EM used for pipeline (not moved to the right location)
   write.table(exprs, file = "${output.tsv}", sep = "\t", quote=FALSE, row.names=FALSE)
 }
-
-
 
 cohort <- NULL
 #-------------------------------
 # PIPELINE
 #-------------------------------
 co <- labkey.setCurlOptions(ssl.verifyhost = 2, sslversion=1)
-labkey.url.base <- jobInfo$value[jobInfo$name == "baseUrl"]
-labkey.url.path <- jobInfo$value[jobInfo$name == "containerPath"]
-FAS_filter <- makeFilter(c("FeatureAnnotationSetId", "IN", "${assay run property, featureSet}")) 
+FAS_filter <- makeFilter(c("FeatureAnnotationSetId/RowId", "IN", "${assay run property, featureSet}")) 
 f2g <- data.table(labkey.selectRows(baseUrl = labkey.url.base,
-                                    folderPath= labkey.url.path,
+                                    folderPath= "/Studies/",
                                     schemaName="Microarray",
                                     queryName="FeatureAnnotation",
                                     colFilter=FAS_filter,
                                     colNameOpt="rname", colSelect = c("featureid", "genesymbol")))
-exprs <- normalizeMatrix(jobInfo, selectedBiosamples)
+if(nrow(f2g) == 0){
+  stop("The downloaded feature annotation set has 0 rows.")
+}
+exprs <- normalizeMatrix(labkey.url.base, labkey.url.path, inputFiles, selectedBiosamples, selectedGEOs)
 bygene <- summarizeMatrix(exprs, f2g)
 
-#exprs <- as.data.frame(exprs)
-#exprs <- cbind(feature_id, exprs)
-#colnames(exprs)[1] <- " "
 
-writeMatrix(exprs, bygene)
+writeMatrix(pipeline.root, exprs, bygene)
 
 outProps = file(description="${pipeline, taskOutputParams}", open="w")
 cat(file=outProps, sep="", "name\tvalue\n")
 cat(file=outProps, sep="", "assay run property, cohort\t", cohort, "\n")
 flush(con=outProps)
 close(con=outProps)
-#cohort_out <- paste0("assay run property, cohort\t", cohort, "\n")
-#write(cohort_out, file="${pipeline,taskOutputParams}")
