91e047b8ccc8548b9b5fa8c8d8c19b083ad8f00e
lwolfe <lwolfe@fredhutch.org>
2019-01-16 00:11:11 +0000

    HIPCCyto: update R script, update xml for new schema

--- HIPCCyto/pipeline/tasks/create-gatingset.R
+++ HIPCCyto/pipeline/tasks/create-gatingset.R
@@ -9,20 +9,59 @@
 library(flowWorkspace)
 library(dplyr)
 
+##----FUNCTIONS----##
 
-##----GET-WS-FILE----##
+.getGS <- function(ws, group_id_num = 1) {
+    gs <- parseWorkspace(ws,
+                         name = group_id_num,
+                         isNCdf = TRUE)
+}
 
-sdy <- "SDY416"
-fcs_dir <- file.path("/share","files", "Studies", sdy, "@files", "rawdata", "flow_cytometry")
-fcs_names <- list.files(fcs_dir, pattern="*.fcs", full.names = TRUE)
+.runData <- function(sdy, ws, gs, group_id) {
+    file_name <- paste0(sdy, "_GS_", group_id, ".rda")
+    workspace <- ws@file
+    num_samples <- length(gs@data@phenoData@data$name)
+    fw_version <- as.character(packageVersion("flowWorkspace"))
 
-# Will have to modify to include .jo
-# FlowWorkspace cannot parse .jo -- will need to pull it and output xml??
-ws_names <- list.files(fcs_dir, pattern="*.xml", full.names = TRUE)
+    run <- data.frame(file_name, workspace, num_samples, fw_version, stringAsFactors = FALSE)
 
-print(paste0("There are ", length(ws_names), " workspace files for ", sdy))
+    return(run)
+}
 
-##----OPEN-WS----##
+
+
+##----PARSE-TASK-INFO----##
+
+taskInfo <- "${pipeline, taskInfo}"
+
+jobInfo <- read.table(taskInfo,
+                      colnames = c("name", "value", "type"),
+                      header = FALSE,
+                      check.names = FALSE,
+                      stringsAsFactors = FALSE,
+                      sep = "\t",
+                      quote = "",
+                      fill = TRUE,
+                      na.strings = "")
+
+labkey.url.path <- jobInfo$value[ jobInfo$name == "containerPath"]
+
+sdy <- gsub("/Studies/", "", labkey.url.path)
+
+##----CHECK-FOR-FILES----##
+# check for workspace file in correct format
+fcs_dir <- file.path("/share", "files", "Studies", sdy, "@files", "rawdata", "flow_cytometry")
+ws_files <- list.files(fcs_dir, pattern = ".xml", full.names = TRUE)
+
+if (length(ws_files) == 0) {
+    stop("No XML workspace files found")
+}
+
+gs_dir <- file.path("/share", "files", "Studies", sdy, "@files", "rawdata", "gating_set")
+
+if (!file.exists(gs_dir)) {
+    dir.create(gs_dir, recursive = TRUE)
+}
 
 ws <- openWorkspace(ws_names)
 
@@ -31,33 +70,16 @@ ws <- openWorkspace(ws_names)
 sample_groups <- getSampleGroups(ws)
 group_ids <- unique(sample_groups$groupID)
 
-##----PARSE-WS----##
-
-## CHANGE TO THIS DIRECTORY
-gs_dir <- file.path("/share","files", "Studies", sdy, "@files", "rawdata", "gating_set")
-gs_files <- list.files(gs_dir, pattern="*.rds", full.names = TRUE)
-
-if (!file.exists(gs_dir)) {
-  dir.create(gs_dir, recursive = TRUE)
+if (length(group_ids) == 1) {
+    gs <- .getGS(ws)
+    run <- .runData(sdy, ws, gs, group_ids)
+    save_gs(gs, gs_dir)
 }
-
-run_data <- lapply(seq(length(group_ids)), FUN = function(group) {
-  gs <- parseWorkspace(ws,
-                       name = group,
-                       isNCdf = TRUE)
-  file_name <- paste0(sdy, "_gs", group, ".rda")
-  workspace <-
-  group_id <- group_ids[group]
-  num_samples <- length(gs@data@phenoData@data$name)
-  fw_version <- packageVersion("flowWorkspace")
-
-  run_data <- data.frame(file_name, num_samples, group_id, fw_version)
-
-  save_gs(gs, gs_dir)
-
-  return(run_data)
-})
-
+# still working on handilng of multiple group id output
+else {
+    stop("multiple group_ids")
+}
 
 ##----WRITE-RUN-DATA-TO-TABLE----##
 
+write.table(run, paste0(gs_dir, "/runs.tsv"), sep = "\t")
