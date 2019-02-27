046e63b72a21f076b95fb8bf757c4c2c86cbac3c
ehenrich <ehenrich@fredhutch.org>
2019-02-27 19:18:33 +0000

    HIPCCyto update schema and bump version

---
+++ HIPCCyto/schemas/dbscripts/postgresql/cytometry_processing-13.50-13.51.sql
@@ -0,0 +1,18 @@
+DROP SCHEMA IF EXISTS cytometry_processing CASCADE;
+CREATE SCHEMA cytometry_processing;
+
+CREATE TABLE cytometry_processing.gatingSetInputFiles
+(
+    Container           ENTITYID    NOT NULL,
+    key                 SERIAL      NOT NULL,
+    file_info_name      TEXT        NOT NULL,
+    ParticipantId       TEXT        NOT NULL,
+    biosample_accession TEXT        NOT NULL,
+    expsample_accession TEXT        NOT NULL,
+    study_accession     TEXT        NOT NULL,
+    wsid                TEXT        NOT NULL,
+    gsdir               TEXT        NOT NULL, 
+    
+    CONSTRAINT PK_gatingSetInputFiles PRIMARY KEY (key),
+    CONSTRAINT UQ_gatingSetInputFiles UNIQUE (Container, file_info_name)
+);    
