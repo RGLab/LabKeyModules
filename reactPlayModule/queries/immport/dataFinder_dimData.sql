ee0e21696b5768aee15c7d0fc924e70d251fa0f5
Helen Miller <hmiller@fredhutch.org>
2019-08-13 15:02:30 -0700

    updated notes

--- reactPlayModule/queries/immport/dataFinder_dimData.sql
+++ reactPlayModule/queries/immport/dataFinder_dimData.sql
@@ -1,3 +1,5 @@
+-- This is an example of a custom query. It is a copy of the query found in the Data Finder module
+
 SELECT DISTINCT subjectid, type as cell_type, timepoint, ('SDY' || split_part(subjectid, '.', 2)) as studyid, assay, timepoint_sortorder
 FROM immport.dimSample
 	LEFT OUTER JOIN immport.dimSampleAssay sa on dimSample.sampleid = sa.sampleid
