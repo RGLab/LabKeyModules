a8f3e8ee9bc38b873107476e4d25fac05ec92b95
Evan Henrich <ehenrich@fredhutch.org>
2020-02-25 16:14:51 -0800

    HIPCMatrix: undoing inputSamplesImmEx change; ETL needed to be run instead

--- HIPCMatrix/queries/study/HM_inputSmplsPlusImmEx.sql
+++ HIPCMatrix/queries/study/HM_inputSmplsPlusImmEx.sql
@@ -1,3 +1,4 @@
+
 SELECT
 GEM.Run,
 GEM.ParticipantId,
@@ -6,9 +7,9 @@ GEM.study_time_collected_unit,
 GEM.cohort,
 GEM.cohort_type,
 GEM.biosample_accession,
-GROUP_CONCAT(DISTINCT immex.exposure_material_reported, ';') AS exposure_material_reported,
-GROUP_CONCAT(DISTINCT immex.exposure_process_preferred, ';') AS exposure_process_preferred,
-FROM 
+immex.exposure_material_reported,
+immex.exposure_process_preferred
+FROM
 (
     SELECT
         *
@@ -17,4 +18,3 @@ FROM
 ) AS GEM
 LEFT JOIN immport.immune_exposure AS immex
 ON immex.subject_accession = SPLIT_PART(GEM.ParticipantId, '.', 1)
-GROUP BY ParticipantId, Run, study_time_collected, study_time_collected_unit, cohort, cohort_type, biosample_accession
