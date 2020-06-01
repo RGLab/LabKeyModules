eb2d118f9ad5dd58c4d006a5d6c2bb2ad94f12a1
Helen Miller <hmiller@fredhutch.org>
2020-06-01 12:28:54 -0700

    Fix study card query

--- DataFinder/resources/queries/immport/dataFinder_studyCard.sql
+++ DataFinder/resources/queries/immport/dataFinder_studyCard.sql
@@ -1,8 +1,8 @@
-SELECT study.study_accession, brief_title, shared_study, restricted, research_focus, P.name as program_title, pi_names, assays, sample_type, container_id
-FROM immport.dimStudy as study
+SELECT dimStudy.study as study_accession, brief_title, shared_study, restricted, research_focus, program as program_title, pi_names, assays, sample_types as sample_type, container_id
+FROM immport.dimStudy
 RIGHT OUTER JOIN 
 	(
 		SELECT Label as study_name, Container as container_id 
 		FROM study.StudyProperties
 	) containers
-	ON study.study_accession = containers.study_name
+	ON dimStudy.study = containers.study_name;
