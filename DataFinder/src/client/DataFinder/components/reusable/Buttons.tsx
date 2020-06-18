5f626c8f81f431a55244b56a34c5019e1c8b14c1
Helen Miller <hmiller@fredhutch.org>
2020-06-17 17:14:01 -0700

    Update scss imports

--- DataFinder/src/client/DataFinder/components/reusable/Buttons.tsx
+++ DataFinder/src/client/DataFinder/components/reusable/Buttons.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import "./Buttons.scss"
 
 interface HighlightedButtonProps {
     label: string;
--- DataFinder/src/client/DataFinder/components/reusable/Dropdowns.tsx
+++ DataFinder/src/client/DataFinder/components/reusable/Dropdowns.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "./Dropdowns.scss"
 
 interface ButtonData {
     label: string;
