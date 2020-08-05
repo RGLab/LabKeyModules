e6387ef7a0de0b45cc991c9f4c3c71ac88bba696
ehenrich <ehenrich@fredhutch.org>
2020-08-05 23:27:37 +0000

    HIPCMatrix: inputSamplesWithFeatures hotfix for container bug

--- HIPCMatrix/assay/ExpressionMatrix/queries/InputSamplesWithFeatures.sql
+++ HIPCMatrix/assay/ExpressionMatrix/queries/InputSamplesWithFeatures.sql
@@ -1,14 +1,26 @@
 SELECT DISTINCT
-  InputSamples_computed.Biosample.biosample_accession AS Biosample,
-  InputSamples_computed.Biosample.participantId,
-  InputSamples_computed.Biosample.study_time_collected,
-  InputSamples_computed.Biosample.study_time_collected_unit,
-  features,
-  featureSet,
+  InputSamplesWithParent.Biosample,
+  InputSamplesWithParent.participantId,
+  InputSamplesWithParent.study_time_collected,
+  InputSamplesWithParent.study_time_collected_unit,
+  rwf.features,
+  rwf.featureSet,
   rwf.runId AS run
-FROM
-  InputSamples_computed 
-JOIN (
+FROM (
+   SELECT DISTINCT
+     InputSamples_computed.Biosample.biosample_accession AS Biosample,
+     InputSamples_computed.Biosample.participantId,
+     InputSamples_computed.Biosample.study_time_collected,
+     InputSamples_computed.Biosample.study_time_collected_unit,
+     InputSamples_computed.Run,
+     C.Parent AS folder_parent
+   FROM
+     InputSamples_computed 
+   LEFT OUTER JOIN
+     core.containers AS C
+   ON
+     folder = C.entityId ) AS InputSamplesWithParent
+LEFT OUTER JOIN (
   SELECT
     currFeatures AS features,
     Runs.featureSet AS featureSet,
@@ -20,7 +32,7 @@ JOIN (
   ON
     Runs.featureSet = origId ) AS rwf
 ON
-  InputSamples_computed.Run = rwf.runId
+  InputSamplesWithParent.Run = rwf.runId
 WHERE
   -- enforce samples only in /Studies, not /HIPC
-  InputSamples_computed.Folder.Parent = 'abbde7af-5b0b-1033-8f62-e818307f16dd'
+  InputSamplesWithParent.folder_parent = 'abbde7af-5b0b-1033-8f62-e818307f16dd'
