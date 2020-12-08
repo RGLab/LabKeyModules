c41a5f484d8f2f440be81ec4d74b9a4eec09072f
Helen Miller <hmiller@fredhutch.org>
2020-12-07 17:45:27 -0800

    Update display

--- DataAccess/webpack/entryPoints.js
+++ DataAccess/webpack/entryPoints.js
@@ -8,7 +8,7 @@ module.exports = {
         // For each webpart, include an object matching
         {
             name: 'DataAccess',
-            frame: 'portal',
+            frame: 'none',
             title: 'Data Access',
             path: './src/client/DataAccess'
         }]
