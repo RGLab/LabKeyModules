b4b0f41d2643e9bf34a641330c85b27a42e12dd5
Helen Miller <hmiller@fredhutch.org>
2020-12-08 15:59:41 -0800

    DataFinder: rm unnecessary comments

--- DataFinder/src/client/DataFinder/helpers/colors.js
+++ DataFinder/src/client/DataFinder/helpers/colors.js
@@ -1,30 +1,7 @@
 // colors for plots are muted versions of colors in selectors and buttons
-export const participantColor =  "#beb9f6" // "#9f97f1" //periwinkle
+export const participantColor =  "#beb9f6" //periwinkle
 export const studyColor = "#bbe2a2" // pistachio
-// export const studyColor = "#f3c450" // "#EFB116" // gold
-// export const sampleColor = "#59cb8f" // "#22ba6a" //green 
 export const sampleColor = "#e8a97f" // amber
-
-// export const samplePalette = [
-//     "#FFFFFF",
-//     "#EDF8E9",
-//     "#C7E9C0",
-//     "#A1D99B",
-//     "#74C476",
-//     "#41AB5D",
-//     "#238B45"
-// ] // original
-
-// green
-// export const samplePalette = [
-//     "#ffffff",
-//     "#e8f8f0",
-//     "#c7eed9",
-//     "#90dcb4",
-//     "#59cb8f",
-//     "#38c179",
-//     "#22ba6a"
-// ] 
 
 // amber
 export const samplePalette = [
