6b90d69cc66f0b740f1d43110f39a7b7d47d7eda
Helen Miller <hmiller@fredhutch.org>
2020-12-09 11:57:49 -0800

    DataAccess: Load only assays with data

--- DataAccess/src/client/DataAccess/DataAccess.tsx
+++ DataAccess/src/client/DataAccess/DataAccess.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Loader} from './components/Loader'
+import { Query} from '@labkey/api';
 
 // Styling imports
 import './DataAccess.scss';
@@ -10,17 +11,6 @@ interface QueryInfo {
     label: string
 }
 
-const datasets: QueryInfo[] = [
-    {schema: "study", query: "ELISA", label: "Enzyme-linked immunosorbent assay (ELISA)"},
-    {schema: "study", query:"ELISPOT", label: "Enzyme-linked immunspot (ELISPOT)"},
-    {schema: "study", query: "HAI", label: "Hemagglutination inhibition (HAI)"},
-    {schema: "study", query: "hla_typing", label: "Human leukocyte antigen (HLA) typing"},
-    {schema: "study", query:"kir_typing", label: "Killer cell immunoglobulin-like receptors (KIR) typing"},
-    {schema: "study", query:"mbaa", label: "Multiplex bead array assay (MBAA)"},
-    {schema: "study", query:"neut_ab_titer", label: "Neutralizing antibody titer"},
-    {schema: "study", query:"pcr", label: "Polymerase chain reaction"}
-]; 
-
 const analyzedResults = [
     {schema: "study", query: "fcs_analyzed_result", label: "Flow cytometry analyzed results"},
     {schema: "gene_expression", query: "DGEA_filteredGEAR", label: "Differential gene expression analysis results"}
@@ -28,56 +18,97 @@ const analyzedResults = [
 
 const demographicsQuery = {schema: "study", query: "demographics", label: "Demographics"}
 
+const getQueries = () => {
+    return new Promise((resolve, reject) => {
+        const res = Query.selectRows({
+            schemaName: 'study',
+            queryName: 'ISC_datasets',
+            failure: (error) => {
+                reject(error.exception) },
+            success: (data) => {
+                console.log(data)
+                resolve(data) }
+        })
+    })
+}
+
 
 export const DataAccess: React.FC = () => {
     const [selectedQuery, setSelectedQuery] = React.useState<QueryInfo>({schema: "study", query: "demographics", label: "Demographics"})
     const [webpartReady, setWebpartReady] = React.useState<Boolean>(false)
+    const [dropdownConfig, setDropdownConfig] = React.useState(null)
     // const queryConfig: QueryConfig = { schemaQuery: SchemaQuery.create(selectedQuery.schema, selectedQuery.query) }
-    
-    const dropdownConfig = {
-        text: "Choose Dataset",
-        items: [
-            {
-                text: "Demographics",
-                handler: () => setSelectedQuery(demographicsQuery)
-            },
-            {
-                text: "Assays",
-                items: datasets.map((queryInfo) => ({
-                    text: queryInfo.label,
-                    handler: () => setSelectedQuery(queryInfo)
-                })
-                )
-            },
-            {
-                text: "Analyzed Results",
-                items: analyzedResults.map((queryInfo) => ({
-                    text: queryInfo.label,
-                    handler: () => setSelectedQuery(queryInfo)
-                })
-                )
-            }
-
-        ]
-    }
     
 
     React.useEffect(() => {
-        new LABKEY.QueryWebPart({
-            renderTo: "data-access-grid",
-            title: selectedQuery.label, 
-            schemaName: selectedQuery.schema,
-            queryName: selectedQuery.query,
-            showRstudioButton: true, 
-            showSurroundingBorder: false,
-            frame: "none",
-            buttonBar: {
-                includeStandardButtons: true,
-                items:[
-                    dropdownConfig
-                    ]},
-            success: () => setWebpartReady(true)
+        getQueries().then((data: any) => {
+            const rows = data.rows
+            const ds = rows.map(row => ({schema: "study", query: row.Name, label: row.Label}))
+            const dc = {
+                text: "Choose Dataset",
+                items: [
+                    {
+                        text: "Demographics",
+                        handler: () => setSelectedQuery(demographicsQuery)
+                    },
+                    {
+                        text: "Assays",
+                        items: ds.map((queryInfo) => ({
+                            text: queryInfo.label,
+                            handler: () => setSelectedQuery(queryInfo)
+                        })
+                        )
+                    },
+                    {
+                        text: "Analyzed Results",
+                        items: analyzedResults.map((queryInfo) => ({
+                            text: queryInfo.label,
+                            handler: () => setSelectedQuery(queryInfo)
+                        })
+                        )
+                    }
+        
+                ]
+            }
+            setDropdownConfig(dc)
+            // initialize QueryWebPart
+            new LABKEY.QueryWebPart({
+                renderTo: "data-access-grid",
+                title: selectedQuery.label, 
+                schemaName: selectedQuery.schema,
+                queryName: selectedQuery.query,
+                showRstudioButton: true, 
+                showSurroundingBorder: false,
+                frame: "none",
+                buttonBar: {
+                    includeStandardButtons: true,
+                    items:[
+                        dc
+                        ]},
+                success: () => setWebpartReady(true)
+            })
         })
+    }, [])
+    
+
+    React.useEffect(() => {
+        if (dropdownConfig != null) {
+            new LABKEY.QueryWebPart({
+                renderTo: "data-access-grid",
+                title: selectedQuery.label, 
+                schemaName: selectedQuery.schema,
+                queryName: selectedQuery.query,
+                showRstudioButton: true, 
+                showSurroundingBorder: false,
+                frame: "none",
+                buttonBar: {
+                    includeStandardButtons: true,
+                    items:[
+                        dropdownConfig
+                        ]},
+                success: () => setWebpartReady(true)
+            })
+        }
     }, [selectedQuery])
 
     // Must return a React Fragment
