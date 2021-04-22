25d56f78faa62314b291ff1fa656a661f422a0b1
Alex <zguo2@fredhutch.org>
2021-04-22 16:44:03 -0700

    removed unnecessary js

--- extraWebapp/src/js/login.js
+++ extraWebapp/src/js/login.js
@@ -2,34 +2,30 @@
 
 import { Utils, Ajax } from "@labkey/api";
 
-export function chkRemember() {
-  // examine cookies to determine if user wants the email pre-populated on form
-  var h = document.getElementById("email");
-  if (h && Utils.getCookie("email")) {
-    h.value = decodeURIComponent(Utils.getCookie("email"));
-  }
+// depreciated because we no longer have a remember email feature
 
-  h = document.getElementById("remember");
-  if (h && Utils.getCookie("email")) {
-    h.checked = true;
-  } else {
-    h.checked = false;
-  }
-}
+// export function chkRemember() {
+//   // examine cookies to determine if user wants the email pre-populated on form
+//   var h = document.getElementById("email");
+//   if (h && Utils.getCookie("email")) {
+//     h.value = decodeURIComponent(Utils.getCookie("email"));
+//   }
+
+//   h = document.getElementById("remember");
+//   if (h && Utils.getCookie("email")) {
+//     h.checked = true;
+//   } else {
+//     h.checked = false;
+//   }
+// }
 
 export function newLogin(setErrorMsg) {
-  // if(document.getElementById('remember').checked == true){
-  //     Utils.setCookie('email', encodeURIComponent(document.getElementById('email').value), true, 360);
-  // } else {
-  //     Utils.deleteCookie('email', true);
-  // }
-  console.log("we starting!");
   const homePageUrl = window.location.href;
+
   Ajax.request({
     url: `${homePageUrl}login/home/loginApi.api`,
     method: "POST",
     params: {
-      //remember: document.getElementById('remember').value,
       remember: false,
       email: document.getElementById("email-sign-in").value,
       password: document.getElementById("password").value,
@@ -39,7 +35,6 @@ export function newLogin(setErrorMsg) {
       if (response && response.returnUrl) {
         window.location = response.returnUrl;
       }
-      //window.location = ActionURL.buildURL('project', 'begin', '/Studies')
       window.location.href = `${homePageUrl}project/Studies/begin.view`;
     }, this),
     failure: Utils.getCallbackWrapper(function (response) {
@@ -51,11 +46,8 @@ export function newLogin(setErrorMsg) {
           setErrorMsg(
             "Invalid Username or Password. You can reset your password via the Forgot Password link above."
           );
-          //   document.getElementById("errors").innerHTML =
-          //     "Invalid Username or Password.<br> You can reset your password via the question mark link above.";
         } else {
           setErrorMsg(response.exception);
-          //   document.getElementById("errors").innerHTML = response.exception;
         }
       }
     }, this),
