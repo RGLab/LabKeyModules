b37ec08dcbf88f1baa3bcebb9fa321e21ff16a0e
guoz24 <59902470+guoz24@users.noreply.github.com>
2021-04-28 12:14:47 -0700

    Fb entrance page integration (#115)
    * updated gitignore
    
    * added new entrance page
    
    * updated jest to 26.6.3
    
    * updated babel-loader to 8.1.0
    
    * updated babel and jest
    
    * converted to typescript and added coming soon tags to analyte explorer
    
    * implemented gradle build
    
    * changed the redirect of all links from "#" to ""
    
    * fixed spelling of site title
    
    * updated login function and error messages
    
    * updated header logo image link to have an id
    
    * removed link from AE image and removed video tutorial section
    
    * updated resources and datafinder links
    
    * added classnames to footer links
    
    * added new descriptions to AE, Resources, and DF. Fixed bug where sign in pop up is cut off on mobile
    
    * fixed all linter errors and 1 security vulnerability
    
    * removed redundant body tag
    
    * removed unnecessary js
    
    * added hover effects to registration buttons, removed second email input for registration module
    
    * refactored out video tutorial block from DOM
    
    * removed debugging statements
    
    * updated readme
    
    * addressed reviewer concerns
    
    * deleted unnecessary logo images
    
    * uninstalled some unnecessary packages
    
    * refactored packages to use top level shared packages more
    
    * fixed favicon reference
    
    * added additional dependency to gradle build file
    
    * Update README.md
    
    * minor styling changes
    
    * additional minor style changes
    
    * Update gradle build for extraWebApp
    
    Co-authored-by: Alex <zguo2@fredhutch.org>
    Co-authored-by: Helen Miller <hmiller@fredhutch.org>

---
+++ extraWebapp/src/js/login.js
@@ -0,0 +1,56 @@
+/* From Matt Bellew @ LabKey, recommended way to login via api June 2017 */
+
+import { Utils, Ajax } from "@labkey/api";
+
+// depreciated because we no longer have a remember email feature
+
+// export function chkRemember() {
+//   // examine cookies to determine if user wants the email pre-populated on form
+//   var h = document.getElementById("email");
+//   if (h && Utils.getCookie("email")) {
+//     h.value = decodeURIComponent(Utils.getCookie("email"));
+//   }
+
+//   h = document.getElementById("remember");
+//   if (h && Utils.getCookie("email")) {
+//     h.checked = true;
+//   } else {
+//     h.checked = false;
+//   }
+// }
+
+export const newLogin = (setErrorMsg) => {
+  Ajax.request({
+    url: `/login/home/loginApi.api`,
+    method: "POST",
+    params: {
+      remember: false,
+      email: document.getElementById("email-sign-in").value,
+      password: document.getElementById("password").value,
+      returnUrl: `/project/Studies/begin.view`,
+    },
+    success: Utils.getCallbackWrapper(function (response) {
+      if (response && response.returnUrl) {
+        window.location = response.returnUrl;
+      }
+      window.location.href = `/project/Studies/begin.view`;
+    }, this),
+    failure: Utils.getCallbackWrapper(function (response) {
+      if (response && response.exception) {
+        // Want special point to password reset not given by LK response
+
+        if (response.exception.includes("did not match")) {
+          setErrorMsg(
+            "Invalid Username or Password. You can reset your password via the Forgot Password link above."
+          );
+        } else {
+          setErrorMsg(response.exception);
+        }
+      }
+    }, this),
+  });
+};
+
+export const newRegistration = () => {
+  window.location.href = `/login/home/register.view?`;
+};
