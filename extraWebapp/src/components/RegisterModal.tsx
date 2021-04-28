0c1472b1d8c6976ebb67a27c2ee1bb7b645a1106
guoz24 <59902470+guoz24@users.noreply.github.com>
2021-04-28 16:03:23 -0700

    Dev to master (#116)
    * Update link in menu (#113)
    
    * Update plumber API path (#114)
    
    * Fb entrance page integration (#115)
    
    * updated gitignore
    
    * added new entrance page
    
    * updated jest to 26.6.3
    
    * updated babel-loader to 8.1.0
    
    * updated babel and jest
    
    * converted to typescript and added coming soon tags to analyte explorer
    
    * implemented gradle build
    
    * changed the redirect of all links from "#" to ""
    
    * fixed spelling of site title
    
    * updated login function and error messages
    
    * updated header logo image link to have an id
    
    * removed link from AE image and removed video tutorial section
    
    * updated resources and datafinder links
    
    * added classnames to footer links
    
    * added new descriptions to AE, Resources, and DF. Fixed bug where sign in pop up is cut off on mobile
    
    * fixed all linter errors and 1 security vulnerability
    
    * removed redundant body tag
    
    * removed unnecessary js
    
    * added hover effects to registration buttons, removed second email input for registration module
    
    * refactored out video tutorial block from DOM
    
    * removed debugging statements
    
    * updated readme
    
    * addressed reviewer concerns
    
    * deleted unnecessary logo images
    
    * uninstalled some unnecessary packages
    
    * refactored packages to use top level shared packages more
    
    * fixed favicon reference
    
    * added additional dependency to gradle build file
    
    * Update README.md
    
    * minor styling changes
    
    * additional minor style changes
    
    * Update gradle build for extraWebApp
    
    Co-authored-by: Alex <zguo2@fredhutch.org>
    Co-authored-by: Helen Miller <hmiller@fredhutch.org>
    
    Co-authored-by: J. Kim <juyeongkim@users.noreply.github.com>
    Co-authored-by: Alex <zguo2@fredhutch.org>
    Co-authored-by: Helen Miller <hmiller@fredhutch.org>

---
+++ extraWebapp/src/components/RegisterModal.tsx
@@ -0,0 +1,200 @@
+import React, { useState } from "react";
+import { newRegistration, newLogin } from "../js/login";
+import { CSSTransition } from "react-transition-group";
+import "./RegisterModal.scss";
+
+interface RegisterModalProps {
+  isOpen: boolean;
+  setRegModalOpen: (arg: boolean) => void;
+}
+
+const RegisterModal: React.FC<RegisterModalProps> = (props) => {
+  const [errorMsg, setErrorMsg] = useState<string>("");
+
+  const closeModal: React.MouseEventHandler<SVGSVGElement> = (
+    e: React.MouseEvent
+  ) => {
+    props.setRegModalOpen(false);
+  };
+
+  const loginFunc = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    newLogin(setErrorMsg);
+  };
+
+  const registerFunc = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    newRegistration();
+  };
+
+  return (
+    <CSSTransition
+      in={props.isOpen}
+      timeout={300}
+      classNames="register"
+      unmountOnExit
+    >
+      <section
+        id="register-modal"
+        className="register register-modal"
+        aria-hidden="true"
+      >
+        <div className="container">
+          <div className="register__content">
+            <header className="register__header">
+              {/* <!-- ?xml version="1.0" encoding="UTF-8"? --> */}
+              <svg
+                className="register__close"
+                viewBox="0 0 18 18"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                onClick={closeModal}
+              >
+                <title>Close button SVG</title>
+                <g
+                  id="Landing-Page"
+                  stroke="none"
+                  strokeWidth="4"
+                  fill="none"
+                  fillRule="evenodd"
+                >
+                  <g
+                    id="Home-Login"
+                    transform="translate(-797.000000, -95.000000)"
+                    stroke="#FFFFFF"
+                  >
+                    <g
+                      id="Group-22"
+                      transform="translate(448.000000, 72.000000)"
+                    >
+                      <g
+                        id="Group-16"
+                        transform="translate(350.000000, 24.000000)"
+                      >
+                        <line
+                          x1="-4.54747351e-13"
+                          y1="4.4408921e-16"
+                          x2="16"
+                          y2="16"
+                          id="Path-9"
+                        ></line>
+                        <line
+                          x1="-4.54747351e-13"
+                          y1="4.4408921e-16"
+                          x2="16"
+                          y2="16"
+                          id="Path-9"
+                          transform="translate(8.000000, 8.000000) scale(-1, 1) translate(-8.000000, -8.000000) "
+                        ></line>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </header>
+            <div className="register__form">
+              <h3 className="register__title">Sign In</h3>
+              <form
+                action="#"
+                onSubmit={loginFunc}
+                className="register__sign-in"
+              >
+                <div style={{ display: "none" }}>
+                  {/* <!-- Hidden input fields may be inserted here --> */}
+                  <input
+                    type="hidden"
+                    name="some-hidden-field"
+                    value="somevalue"
+                  />
+                </div>
+                <div className="input-wrap input-wrap__column">
+                  <label className="label" htmlFor="email">
+                    Email
+                  </label>
+                  <span className="email">
+                    <input
+                      type="text"
+                      name="email"
+                      defaultValue=""
+                      size={40}
+                      className=""
+                      id="email-sign-in"
+                      aria-required="true"
+                      aria-invalid="false"
+                    />
+                  </span>
+                  <div className="input-wrap__forgot-password">
+                    <label className="label label-password" htmlFor="password">
+                      Password
+                    </label>
+                    <a
+                      href={`${window.location.href}login/home/resetpassword.view`}
+                      target="_self"
+                      title="Forgot Password"
+                      className="forgot-password"
+                    >
+                      Forgot Password?
+                    </a>
+                  </div>
+                  <span className="password">
+                    <input
+                      type="password"
+                      name="password"
+                      defaultValue=""
+                      size={40}
+                      className=""
+                      id="password"
+                      aria-required="true"
+                      aria-invalid="false"
+                    />
+                  </span>
+                  {errorMsg !== "" && <span id="error">{`${errorMsg}`}</span>}
+                </div>
+                <button className="submit-btn" type="submit">
+                  Sign In
+                </button>
+              </form>
+              <form
+                action="#"
+                onSubmit={registerFunc}
+                className="register__register"
+              >
+                <div style={{ display: "none" }}>
+                  {/* <!-- Hidden input fields may be inserted here --> */}
+                  <input
+                    type="hidden"
+                    name="some-hidden-field"
+                    value="somevalue"
+                  />
+                </div>
+                <div className="input-wrap input-wrap__column">
+                  <label className="label" htmlFor="email">
+                    First Time User?
+                  </label>
+                  {/* <span className="email">
+                    <input
+                      type="text"
+                      name="email"
+                      defaultValue=""
+                      size={40}
+                      className=""
+                      id="email-register"
+                      aria-required="true"
+                      aria-invalid="false"
+                    />
+                  </span> */}
+                </div>
+                <button className="register-btn" type="submit">
+                  Register
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </CSSTransition>
+  );
+};
+
+export default RegisterModal;
