f00735c7c6d9f780d27117738ec87cb77f9da729
Alex Guo <zguo2@fredhutch.org>
2021-06-01 11:13:53 -0700

    set up analyte explorer module folder

---
+++ AnalyteExplorer/src/client/AnalyteExplorer/AnalyteExplorer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnalyteExplorer: React.FC = () => {
+  return <h1>Hello</h1>;
+};
+
+const App: React.FC = () => {
+  return <AnalyteExplorer />;
+};
+
+export default App;
---
+++ AnalyteExplorer/src/client/AnalyteExplorer/app.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from "./AnalyteExplorer";
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("analyte-explorer")
+  );
+});
