eef4df1d91001c024304173c29c140a3d7c895dc
Alex Guo <59902470+guoz24@users.noreply.github.com>
2021-06-23 16:25:10 -0700

    Fb spring cleaning (#128)
    * updated gitignore
    
    * refactored all svgs (#118)
    
    * Fb linkable tabs (#123)
    
    * installed react-router-dom
    
    * implemented linkable tabs
    
    * added comments
    
    * created linkable tabs for Resources page
    
    * Fb update styles about resources (#122)
    
    * updated immunespace theme in package.json
    
    * created nav indicator
    
    * started style changes About Page
    
    * added comments
    
    * minor path edit
    
    * uninstalled react-router-dom
    
    * refactored all svgs (#124)
    
    * Fb spring cleaning refactor css (#125)
    
    * refactored cognition css
    
    * updated pathing and deleted old scss
    
    * points to the new immunespace theme package
    
    * Fb spring cleaning vulnerability fixes (#126)
    
    * fixed 7/8 vulnerabilities
    
    * upgraded resolve-url-loader to 4.0.0
    
    * changed navbar background color to white
    
    * fixed bug where page content was out of synch with url.
    
    * added comments and additional typing
    
    * last minute code refactor and adding social media sites
    
    * fixed bug where initial page load introduces duplicate history entries. Changed link of About Us on homepage to point to ?tab=About
    
    * reconfigured tab indicator to slide when nav item is clicked. Fixed bug where backspace would not render nav item tabs
    
    * removed console log
    
    * applied PR comments
    
    * Dev (#138)
    
    * Update LabKeyModules path
    
    * Fix typo and remove whitespace
    
    * Display DataExplorer in study level folders
    
    * Update software updates page
    
    Co-authored-by: J. Kim <jkim2345@fredhutch.org>
    
    * changed gene expression tab name
    
    * Fix npm dependency vulnerabilities
    
    Co-authored-by: J. Kim <jkim2345@fredhutch.org>
    Co-authored-by: Helen MIller <hmiller@fredhutch.org>

--- ResourcesPage/src/client/ResourcesPage/MostCited.tsx
+++ ResourcesPage/src/client/ResourcesPage/MostCited.tsx
@@ -1,90 +1,116 @@
 import * as React from "react";
-import {DropdownButton, MenuItem} from 'react-bootstrap';
-import {BarPlot, BarPlotDatum} from './PlotComponents/mostCitedBarPlot'
-import {updatePmPlotData} from './MostCited/utils'
-import {DROPDOWN_OPTIONS} from './MostCited/constants'
+import { DropdownButton, MenuItem } from "react-bootstrap";
+import { BarPlot, BarPlotDatum } from "./PlotComponents/mostCitedBarPlot";
+import { updatePmPlotData } from "./MostCited/utils";
+import { DROPDOWN_OPTIONS } from "./MostCited/constants";
 
 interface props {
-    transformedPmData: {
-        byPubId: BarPlotDatum[]
-    };
-    pmDataRange: {
-        byPubId: number[]
-    };
+  transformedPmData: {
+    byPubId: BarPlotDatum[];
+  };
+  pmDataRange: {
+    byPubId: number[];
+  };
 }
 
-export const MostCited = React.memo<props>(( {transformedPmData, pmDataRange}: props) => {
-    const [pmOrderBy, setPmOrderBy] = React.useState(DROPDOWN_OPTIONS[0].value)
+export const MostCited = React.memo<props>(
+  ({ transformedPmData, pmDataRange }: props) => {
+    const [pmOrderBy, setPmOrderBy] = React.useState(DROPDOWN_OPTIONS[0].value);
 
     interface plotProps {
-        transformedPmData: {
-            byPubId: BarPlotDatum[]
-        };
-        pmDataRange: {
-            byPubId: number[]
-        };
-        pmOrderBy: string;
+      transformedPmData: {
+        byPubId: BarPlotDatum[];
+      };
+      pmDataRange: {
+        byPubId: number[];
+      };
+      pmOrderBy: string;
     }
 
-    function onSelectChangeOrder(eventKey){
-        setPmOrderBy(eventKey)
+    function onSelectChangeOrder(eventKey) {
+      setPmOrderBy(eventKey);
     }
 
     const getDropDown = React.useCallback(() => {
-        return(
-            <div>
-                <DropdownButton title='Select Order' id='order-select-dropdown'>
-                    <MenuItem eventKey={DROPDOWN_OPTIONS[0].value} onSelect={onSelectChangeOrder}>
-                        {DROPDOWN_OPTIONS[0].label}
-                    </MenuItem>
-                    <MenuItem eventKey={DROPDOWN_OPTIONS[1].value} onSelect={onSelectChangeOrder}>
-                        {DROPDOWN_OPTIONS[1].label}
-                    </MenuItem>
-                    <MenuItem eventKey={DROPDOWN_OPTIONS[2].value} onSelect={onSelectChangeOrder}>
-                        {DROPDOWN_OPTIONS[2].label}
-                    </MenuItem>
-                </DropdownButton>
-            </div>
-        )
-    }, [])
+      return (
+        <div>
+          <DropdownButton title="Select Order" id="order-select-dropdown">
+            <MenuItem
+              eventKey={DROPDOWN_OPTIONS[0].value}
+              onSelect={onSelectChangeOrder}
+            >
+              {DROPDOWN_OPTIONS[0].label}
+            </MenuItem>
+            <MenuItem
+              eventKey={DROPDOWN_OPTIONS[1].value}
+              onSelect={onSelectChangeOrder}
+            >
+              {DROPDOWN_OPTIONS[1].label}
+            </MenuItem>
+            <MenuItem
+              eventKey={DROPDOWN_OPTIONS[2].value}
+              onSelect={onSelectChangeOrder}
+            >
+              {DROPDOWN_OPTIONS[2].label}
+            </MenuItem>
+          </DropdownButton>
+        </div>
+      );
+    }, []);
 
     const getContent = React.useCallback(() => {
-        const pmPlotData = updatePmPlotData(transformedPmData, pmDataRange, pmOrderBy)
-        if(typeof(pmPlotData) !== "undefined"){
-            return(
-                <BarPlot
-                    data={pmPlotData.data} 
-                    titles={pmPlotData.titles}
-                    name={pmPlotData.name} 
-                    height={pmPlotData.height} 
-                    width={pmPlotData.width} 
-                    dataRange={pmPlotData.dataRange}
-                    linkBaseText={pmPlotData.linkBaseText}
-                />
-            )
-        }else{
-            return(
-                <div>
-                    <i aria-hidden="true" className="fa fa-spinner fa-pulse" style={{marginRight:'5px'}}/>
-                    Loading plots ...
-                </div>
-            )
-        }
-        
-    },[pmOrderBy])
+      const pmPlotData = updatePmPlotData(
+        transformedPmData,
+        pmDataRange,
+        pmOrderBy
+      );
 
-    return(
-        <div id="#most-cited">
-            <h2>Most Cited Publications Related to Studies to ImmuneSpace</h2>
-            <p><b>For More Information:</b></p>
-            <ul>
-                <li>Hover over each bar for publication information</li>
-                <li>Click on the Y-axis label to go to PubMed page for the publication</li>
-                <li>Update the ordering of the publications using the dropdown menu below</li>
-            </ul>
-            <br></br>
-            {getDropDown()}
-            {getContent()}
-        </div>
-    )
-})
+      if (typeof pmPlotData !== "undefined") {
+        return (
+          <BarPlot
+            data={pmPlotData.data}
+            titles={pmPlotData.titles}
+            name={pmPlotData.name}
+            height={pmPlotData.height}
+            width={pmPlotData.width}
+            dataRange={pmPlotData.dataRange}
+            linkBaseText={pmPlotData.linkBaseText}
+          />
+        );
+      } else {
+        return (
+          <div>
+            <i
+              aria-hidden="true"
+              className="fa fa-spinner fa-pulse"
+              style={{ marginRight: "5px" }}
+            />
+            Loading plots ...
+          </div>
+        );
+      }
+    }, [transformedPmData, pmDataRange, pmOrderBy]); // updated so that the plot updates when the data changes.
+
+    return (
+      <div id="#most-cited">
+        <h2>Most Cited Publications Related to Studies to ImmuneSpace</h2>
+        <p>
+          <b>For More Information:</b>
+        </p>
+        <ul>
+          <li>Hover over each bar for publication information</li>
+          <li>
+            Click on the Y-axis label to go to PubMed page for the publication
+          </li>
+          <li>
+            Update the ordering of the publications using the dropdown menu
+            below
+          </li>
+        </ul>
+        <br></br>
+        {getDropDown()}
+        {getContent()}
+      </div>
+    );
+  }
+);
