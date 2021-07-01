23c216e4ec884d025bbde12e670d34cdbcf54105
Alex Guo <zguo2@fredhutch.org>
2021-07-01 15:54:57 -0700

    fun changes

--- AnalyteExplorer/src/client/AnalyteExplorer/components/AnalyteSelectorMain.tsx
+++ AnalyteExplorer/src/client/AnalyteExplorer/components/AnalyteSelectorMain.tsx
@@ -1,16 +1,40 @@
 import React from "react";
+import { AiTwotoneSafetyCertificate } from "react-icons/ai";
 import "./AnalyteSelectorMain.scss";
 
 const AnalyteSelectorMain = () => {
   const [selected, setSelected] = React.useState(0); //0 means nothing is selected
+
+  const testRef = React.useRef(null);
+
+  const openDropdown = (e: React.MouseEvent, selectorNum: number) => {
+    console.log(testRef);
+    console.log(e.target);
+    setSelected(selectorNum);
+  };
+
+  const closeDropdown = (e: any) => {
+    if (e.target !== testRef.current) {
+      setSelected(0);
+    }
+  };
+
+  React.useEffect(() => {
+    if (selected > 0) {
+      document.addEventListener("click", closeDropdown);
+    } else {
+      document.removeEventListener("click", closeDropdown);
+    }
+  });
 
   return (
     <div className="analyte-selector-main">
       <div className="analyte-selector-dropdown">
         <div className="analyte-selector">
           <div
+            ref={testRef}
             className="analyte-selector-label-wrapper"
-            onClick={() => setSelected(1)}>
+            onClick={(e) => openDropdown(e, 1)}>
             <div>
               <label htmlFor="analyte-type" className="analyte-selector-label">
                 Analyte Type
@@ -53,24 +77,26 @@ const AnalyteSelectorMain = () => {
           </div>
         </div>
       </div>
-      <div
-        className="analyte-selector-dropdown-menu"
-        style={{ marginLeft: `${(selected % 4) * 100}px` }}>
-        <div className="analyte-selector-dropdown-content">
-          <div className="analyte-selector-dropdown-options">
-            <span>Option + {selected}</span>
-          </div>
-          <div className="analyte-selector-dropdown-options">
-            <span>Option</span>
-          </div>
-          <div className="analyte-selector-dropdown-options">
-            <span>Option</span>
-          </div>
-          <div className="analyte-selector-dropdown-options">
-            <span>Option</span>
-          </div>
-          <div className="analyte-selector-dropdown-options">
-            <span>Option</span>
+      <div className="mock-outside">
+        <div
+          className="analyte-selector-dropdown-menu"
+          style={{ marginLeft: `${(selected % 4) * 100}px` }}>
+          <div className="analyte-selector-dropdown-content">
+            <div className="analyte-selector-dropdown-options">
+              <span>Option + {selected}</span>
+            </div>
+            <div className="analyte-selector-dropdown-options">
+              <span>Option</span>
+            </div>
+            <div className="analyte-selector-dropdown-options">
+              <span>Option</span>
+            </div>
+            <div className="analyte-selector-dropdown-options">
+              <span>Option</span>
+            </div>
+            <div className="analyte-selector-dropdown-options">
+              <span>Option</span>
+            </div>
           </div>
         </div>
       </div>
