98eac337bd59435f0f944f89869e8feae58e82d0
Helen Miller <hmiller@fredhutch.org>
2021-08-11 10:50:04 -0700

    Add link to HIPCMatrix package in About page

--- AboutPage/src/client/AboutPage/GeneExpression.tsx
+++ AboutPage/src/client/AboutPage/GeneExpression.tsx
@@ -7,6 +7,11 @@ export const GeneExpression = () => {
             <img src="/AboutPage/images/ge_standardization.png"
                  padding-top="80%"
                  width="65%"/>
+            <h2>Details</h2>
+            <p>
+                Bulk gene expression processing and standardization in ImmuneSpace is 
+                facilitated by the <a href='https://github.com/RGLab/HIPCMatrix'>HIPCMatrix</a> R package. 
+            </p>
         </div>
     )
 }
