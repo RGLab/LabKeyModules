e9e37afe9092218f2c5174887c4beb562510840a
Alex Guo <zguo2@fredhutch.org>
2021-09-16 12:26:23 -0700

    finished jest setup

--- AnalyteExplorer/test/helperFunctions.test.ts
+++ AnalyteExplorer/test/helperFunctions.test.ts
@@ -1,5 +1,21 @@
-import { capitalizeFirstLetter } from "../src/client/AnalyteExplorer/helpers/helperFunctions";
+import {
+  capitalizeFirstLetter,
+  getAverage,
+  capitalizeKebabCase,
+  capitalizeEveryWord,
+} from "../src/client/AnalyteExplorer/helpers/helperFunctions";
 
 test("capitalize ab", () => {
   expect(capitalizeFirstLetter("ab")).toBe("Ab");
+});
+
+test("test average", () => {
+  expect(getAverage([0])).toBe(0);
+  expect(getAverage([1, 2])).toBe(1.5);
+  expect(getAverage([1, 2, 3])).toBe(2);
+  expect(getAverage([1, -1])).toBe(0);
+  expect(getAverage([-1, -1])).toBe(-1);
+  expect(getAverage(undefined)).toBe(null);
+  expect(getAverage(null)).toBe(null);
+  //expect(getAverage([])).toBe(null);
 });
