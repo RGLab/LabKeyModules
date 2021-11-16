99b8678d6ae1c563f23cc45add70284d43f6b93e
J. Kim <juyeongkim@users.noreply.github.com>
2021-11-15 18:02:00 -0800

    Build and push rstudio images to Docker Hub

--- Scripts/installR.R
+++ Scripts/installR.R
@@ -5,9 +5,9 @@ if (length(args) == 0) {
   stop("Argument must be supplied (BUILD_TYPE)")
 }
 
-install.packages("Rserve", , "http://cran.fhcrc.org") # install.packages("Rserve", , "http://www.rforge.net/")
+install.packages("Rserve", repos = "https://cran.rstudio.com/")
 
-install.packages("BiocManager", , "http://cran.fhcrc.org")
+install.packages("BiocManager", repos = "https://cran.rstudio.com/")
 BiocManager::install(c(
   "remotes",
   scan("listOfNeededRPackages", what = "character"),
@@ -17,10 +17,16 @@ BiocManager::install(c(
 remotes::install_github(c(
   paste0("RGLab/ImmuneSpaceR@", args[1]),
   paste0("RGLab/UpdateAnno@", args[1]),
-  "RGLab/ImmuneSignatures"
-))
+  "RGLab/ImmuneSignatures",
+  paste0("RGLab/HIPCMatrix@", args[1]),
+  "RGLab/HIPCCyto",
+  "RGLab/AnalyteExplorer",
+  "RGLab/ImmPortR"
+), dependencies = TRUE)
 
 # Install custom annotation packages
 remotes::install_url(paste0("https://github.com/RGLab/UpdateAnno/raw/", args[1], "/FeatureAnnotationSetDev/hursta2a520709cdf.tar.gz"))
 remotes::install_url(paste0("https://github.com/RGLab/UpdateAnno/raw/", args[1], "/FeatureAnnotationSetDev/huex10stv2cdf.tar.gz"))
 
+remotes::install_url("https://cran.r-project.org/src/contrib/Archive/pheatmap/pheatmap_1.0.8.tar.gz")
+remotes::install_url("https://www.bioconductor.org/packages//2.10/bioc/src/contrib/DESeq_1.8.3.tar.gz")
