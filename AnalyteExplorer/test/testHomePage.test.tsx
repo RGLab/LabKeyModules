9e1d1cec843ee5b4e49044ac2da7bac4cc2f74ff
Alex Guo <zguo2@fredhutch.org>
2022-01-11 15:30:22 -0800

    Fix HomePage tests

--- AnalyteExplorer/test/testHomePage.test.tsx
+++ AnalyteExplorer/test/testHomePage.test.tsx
@@ -3,10 +3,9 @@
  */
 
 import React from "react";
-import { shallow, render, mount } from "enzyme";
-import HomePage, {
-  ArrowText,
-} from "../src/client/AnalyteExplorer/components/HomePage";
+import { shallow } from "enzyme";
+import HomePage from "../src/client/AnalyteExplorer/components/HomePage";
+import renderer from "react-test-renderer";
 
 describe("<HomePage />", () => {
   it("shallow renders <HomePage />", () => {
@@ -16,22 +15,11 @@ describe("<HomePage />", () => {
     expect(wrapper.find(".ae-home__tutorial-icon")).toHaveLength(3);
     expect(wrapper.find(".ae-home-tutorial-instructions")).toHaveLength(1);
     expect(wrapper.find(".ae-home-data-processing")).toHaveLength(1);
-
-    //expect(wrapper.find(".ae-home-arrow-text")).toHaveLength(1);
   });
 
-  it("shallow renders <ArrowText />", () => {
-    const text = "Hello";
-    const wrapper = render(<ArrowText text={text} />);
-    //expect(wrapper.find("a")).toHaveLength(1); // can't find the a tag for some reason
-    expect(wrapper.find(".history-block__link-text")).toHaveLength(1);
-    expect(wrapper.find(".history-block__link-text").text()).toEqual(text);
-  });
-
-  it("full rendering ArrowText", () => {
-    const text = "Hello";
-    const wrapper = mount(<ArrowText text={text} />);
-    expect(wrapper.find(".ae-home-arrow-text")).toHaveLength(1);
-    //expect(wrapper.find(".history-block__link-text::after")).toHaveLength(1);
+  it("HomePage snapshot test", () => {
+    const component = renderer.create(<HomePage />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
