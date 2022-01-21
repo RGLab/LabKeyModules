84badf8fbe4d2386594eb066910670ba8472bb5e
Alex Guo <zguo2@fredhutch.org>
2022-01-21 14:24:34 -0800

    Remove &nbsp between lists in SoftwareUpdates

--- AboutPage/src/client/AboutPage/SoftwareUpdates.tsx
+++ AboutPage/src/client/AboutPage/SoftwareUpdates.tsx
@@ -16,61 +16,61 @@ export const SoftwareUpdates = () => {
                     and analysis.
                 </li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>30 June 2021</b></p>
             <ul>
                 <li>Upgraded to latest 21.6 version of LabKey</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>10 May 2021</b></p>
             <ul>
                 <li>Upgraded to latest <a href="https://www.labkey.org/Documentation/wiki-page.view?name=whatsnew213">21.3 version</a> of LabKey</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>28 April 2021</b></p>
             <ul>
                 <li>Updated entrance page</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>11 January 2021</b></p>
             <ul>
                 <li>Upgraded to latest <a href="https://www.labkey.org/Documentation/wiki-page.view?name=whatsnew2011">20.11 version</a> of LabKey</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>9 December 2020</b></p>
             <ul>
                 <li>Rolled out <a href="https://twitter.com/ImmuneSpace/status/1336717242340413440">new logo</a></li>
                 <li>Upgrade to ImmuneSpaceR version 1.18.1</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>3 August 2020</b></p>
             <ul>
                 <li>Launched <a href="https://twitter.com/ImmuneSpace/status/1290290316604395520">new Data Finder</a></li>
                 <li>Rolled out <a href="https://twitter.com/ImmuneSpace/status/1290290329887752195">Resources page</a></li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>10 June 2020</b></p>
             <ul>
                 <li>Upgraded to latest <a href="https://www.labkey.org/Documentation/wiki-page.view?name=whatsnew203">20.3 version</a> of LabKey</li>
                 <li>Upgraded Java to openJDK-14</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>6 January 2020</b></p>
             <ul>
                 <li>Upgraded to latest <a href="https://www.labkey.org/Documentation/wiki-page.view?name=whatsnew193">19.3 version</a> of LabKey</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>26 Dec 2019</b></p>
             <ul>
                 <li>Upgraded to <a href="https://www.labkey.org/wiki/Documentation/Archive/19.2/page.view?name=releasenotes192">LabKey 19.2</a></li>
                 <li>Upgrade to ImmuneSpaceR version 1.13.2</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>21 May 2019</b></p>
             <ul>
                 <li>Upgraded R to 3.6.0</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>20 May 2019</b></p>
             <ul>
                 <li>Upgraded to latest <a href="https://www.labkey.org/Documentation/wiki-page.view?name=whatsnew191">19.1 version</a> of LabKey.</li>
@@ -78,12 +78,12 @@ export const SoftwareUpdates = () => {
                 <li>Upgraded Tomcat to Tomcat 9.0.17</li>
                 <li>Upgraded Commons Daemon to 1.1.0</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>4 March 2019</b></p>
             <ul>
                 <li>Upgraded R to 3.5.2</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>27 February 2019</b></p>
             <ul>
                 <li>Upgraded to latest <a href="https://www.labkey.org/Documentation/wiki-page.view?name=whatsnew183">18.3 version</a> of LabKey.</li>
@@ -96,13 +96,13 @@ export const SoftwareUpdates = () => {
                     </ul>
                 </li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>15 August 2018</b></p>
             <ul>
                 <li>Upgraded to latest <a href="https://www.labkey.org/Documentation/wiki-page.view?name=whatsnew182">18.2 version</a> of LabKey.</li>
                 <li>Upgraded to <a href="https://github.com/RGLab/ImmuneSpaceR">ImmuneSpaceR</a> version 1.7.4</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>4 May 2018</b></p>
             <ul>
                 <li>Upgraded to latest <a href="https://www.labkey.org/Documentation/wiki-page.view?name=whatsnew181">18.1 version</a> of LabKey (e.g. sharing of reports as well as editing them via the integrated RStudio Server is now available).</li>
@@ -115,65 +115,65 @@ export const SoftwareUpdates = () => {
                     </ul>
                 </li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>6 February 2018</b></p>
             <ul>
                 <li>Upgraded to latest 17.3 version of LabKey.</li>
                 <li>Upgraded R to 3.4.3</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>27 November 2017</b></p>
             <ul>
                 <li>Upgraded to latest 17.2 version of LabKey.</li>
                 <li>Upgraded custom modules, including error handling in HIPCMatrix for CreateMatrix functionality.</li>
                 <li>Upgraded to ImmuneSpaceR version 1.7.0 that utilizes a dynamic Gene Expression query.</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>24 August 2017</b></p>
             <ul>
                 <li>Upgrade to LabKey 17.2.</li>
                 <li>Added functionality to automatically update and standardize the gene symbols used on the portal.</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>16 June 2017</b></p>
             <ul>
                 <li>Upgrade to R 3.4.0.</li>
                 <li>Enabled interactive visualizations for all reports and modules.</li>
                 <li>Added to the Data Finder an ability to filter individual studies.</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>24 March 2017</b></p>
             <ul>
                 <li>Upgrade to LabKey 17.1.</li>
                 <li>Added the ability to launch an integrated <a title="RStudio Server" href="https://www.rstudio.com/products/rstudio-server/">RStudio Server</a> instance.</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>13 December 2016</b></p>
             <ul>
                 <li>Upgrade to R 3.3.1.</li>
                 <li>Upgrade to LabKey 16.3.</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>21 July 2016</b></p>
             <ul>
                 <li>Upgrade to LabKey 16.2.</li>
                 <li>Enabled markdown v2 for all reports and modules.</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>3 May 2016</b></p>
             <ul>
                 <li>A new export interface as been added to the <a href="https://www.immunespace.org/project/Studies/begin.view?">Data Finder</a> to download selected files and tables as. Click the <strong>Export study datasets</strong> button to access it.</li>
                 <li>Upgrade to R 3.2.4.</li>
                 <li>Upgrade to LabKey 16.1.</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>22 October 2015</b></p>
             <ul>
                 <li>A new <a href="https://www.immunespace.org/project/Studies/begin.view?">Data Finder</a> replaces the study finder.</li>
                 <li>Upgrade to LabKey 15.3.</li>
                 <li>The <a href="https://www.immunespace.org/DataExplorer/Studies/begin.view">Data Explorer</a> can now be used to plot data across studies.</li>
             </ul>
-            <p>&nbsp;</p>
+            
             <p><b>22 July 2015</b></p>
             <ul>
                 <li>Upgrade to LabKey 15.2.</li>
